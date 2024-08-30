fa4ebfeb92da6dc7e664822bdb70670f1898e2e9
Amanda Johnson <amanda.j86@gmail.com>
2024-08-30 00:06:43 -0700

    navigator 1 and 2 update

---
+++ screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'; //this line is not in the nucamp exercise code
+import { Text, View } from 'react-native';
+
+const HomeScreen = () => {
+    return (
+        <View>
+            <Text>Home Screen</Text>
+        </View>
+    );
+};
+
+export default HomeScreen;
