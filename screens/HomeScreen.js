e903a28de1e2a22a82a5d6f3ce3b287bf0b20232
Amanda Johnson <amanda.j86@gmail.com>
2024-08-30 20:01:36 -0700

    navigation added

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,57 @@
 import React from 'react'; //this line is not in the nucamp exercise code
-import { Text, View } from 'react-native';
+import { useState } from 'react';
+import { ScrollView, Text, View } from 'react-native';
+import { Card } from 'react-native-elements';
+import { CAMPSITES } from '../shared/campsites';
+//import { CAMPSITES } from '../shared/campsites';
+import { PROMOTIONS } from '../shared/promotions';
+import { PARTNERS } from '../shared/partners';
+
+const FeaturedItem = ({ item }) => {
+    if (item) {
+        return (
+            <Card containerStyle={{ padding: 0 }}>
+                <Card.Image source={item.image}>
+                    <View style={{ justifyContent: 'center', flex: 1 }}>
+                        <Text
+                            style={{
+                                color: 'white',
+                                textAlign: 'center',
+                                fontSize: 20
+                            }}
+                        >
+                            {item.name}
+                        </Text>
+                    </View>
+                </Card.Image>
+                <Text style={{ margin: 20 }}>{item.description}</Text>
+            </Card>
+        );
+    }
+    return <View />;
+};
 
 const HomeScreen = () => {
+  console.log('PARTNERS', partners)
+    const [campsites, setCampsites] = useState(CAMPSITES);
+    const [promotions, setPromotions] = useState(PROMOTIONS);
+    const [partners, setPartners] = useState(PARTNERS);
+
+    // Add console.log statements to check the contents of the state variables
+    console.log('Campsites:', campsites);
+    console.log('Promotions:', promotions);
+    console.log('Partners:', partners);
+
+    const featCampsite = campsites.find((item) => item.featured);
+    //const featPromotion = promotions.find((item) => item.featured);
+    const featPartner = partners.find((item) => item.featured);
+
     return (
-        <View>
-            <Text>Home Screen</Text>
-        </View>
+        <ScrollView>
+            <FeaturedItem item={featCampsite} />
+            {/*<FeaturedItem item={featPromotion} />*/}
+            <FeaturedItem item={featPartner} />
+        </ScrollView>
     );
 };
 
